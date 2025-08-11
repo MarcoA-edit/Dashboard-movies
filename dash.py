c896789da63857e372ee977efd72ff8e983fda33
Marco Antônio Gonçalves Lopes <marco.edit01@gmail.com>
2025-08-11 16:32:13 -0300

    dash.py

--- dash.py
+++ dash.py
@@ -187,7 +187,7 @@ with t2:
                 response.raise_for_status()
                 img = Image.open(BytesIO(response.content))
 
-                resized_img = img.resize((500, 600))
+                resized_img = img.resize((700, 600))
 
                 m1.image(resized_img, use_container_width=False)
             except Exception as e:
@@ -448,3 +448,4 @@ with t2:
                 #popb2.error(f"Erro inesperado: {e}")
         #else:
             #popb2.warning("Por favor, escreva algo antes de salvar.")
+
